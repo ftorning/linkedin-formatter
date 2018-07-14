6724b118ce0d1b066ddaaa0667d292ca3c4b01b2
Torning, Fraser <Fraser_Torning@comcast.com>
2018-07-14 10:05:59 -0400

    init commit

---
+++ assets/js/credentials.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBqorYznKywT9i3WRV42AhFtBsYJBWoXC0",
+    authDomain: "project-1-a76c3.firebaseapp.com",
+    databaseURL: "https://project-1-a76c3.firebaseio.com",
+    projectId: "project-1-a76c3",
+    storageBucket: "",
+    messagingSenderId: "496672208152"
+  };
